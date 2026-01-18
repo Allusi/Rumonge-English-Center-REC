5f25cfbe33194df7a410fec7d2a9d7e58cef0872
Allu Simeon <allusimeon57@gmail.com>
2026-01-18 14:11:58 +0000

    This function has crashed An unhandled error in the function code trigge

--- src/app/api/flows/[...flow]/route.ts
+++ src/app/api/flows/[...flow]/route.ts
@@ -1,7 +1,7 @@
-import nextHandler from '@genkit-ai/next';
+import { run } from '@genkit-ai/next';
 
 // This is the entry point that registers all of our flows.
 // It's also used by the genkit dev server.
 import '@/ai/dev';
 
-export const { GET, POST, OPTIONS } = nextHandler();
+export { run as GET, run as POST, run as OPTIONS };
