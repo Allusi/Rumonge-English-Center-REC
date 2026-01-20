2fcb0f8dcd21bf5e0394166d650e34ab75a239db
Allu Simeon <allusimeon57@gmail.com>
2026-01-20 11:50:10 +0000

    Make app cache to work offline when no Internet is available  entirely

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const withPWA = require('next-pwa')({
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
   sw: 'sw.js',
-  
+  runtimeCaching: require('next-pwa/cache'),
 });
 
 
